7a0cb849d85db932db761603dba9354e6fdbc7ff
MikovozSergey <mikovozs98@gmail.com>
2020-02-24 23:04:28 +0300

    add icons add StartView

--- CarsApp/MainViewController.swift
+++ CarsApp/MainViewController.swift
@@ -38,7 +38,7 @@ class MainViewController: UITableViewController {
     // MARK: - Table view delegate
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
+        return 90
     }
     
     /*
