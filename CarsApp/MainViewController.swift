54c5370928a7e3eb861e0a529722f6993e563289
MikovozSergey <mikovozs98@gmail.com>
2020-02-24 23:03:21 +0300

    add icons add StartView

--- CarsApp/MainViewController.swift
+++ CarsApp/MainViewController.swift
@@ -29,8 +29,16 @@ class MainViewController: UITableViewController {
         
         cell.textLabel?.text = carNames[indexPath.row]
         cell.imageView?.image = UIImage(named: carNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
+        cell.imageView?.clipsToBounds = true
         
         return cell
+    }
+    
+    // MARK: - Table view delegate
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
     
     /*
